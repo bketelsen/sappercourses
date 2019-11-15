a1c3024c529fb8b070d0d4f0e206a1fb207cb46e
Brian Ketelsen <mail@bjk.fyi>
2019-11-15 14:40:07 -0500

    first commit

---
+++ ssg.config.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+const yaml = require('@ssgjs/source-yaml').default;
+
+exports.plugins = {
+  yamlFiles: yaml({ dirPath: 'data' })
+};
+
+let eventsData = null;
+// optional. called repeatedly, can be expensive
+exports.getDataSlice = async (key, uid) => {
+  console.log('optional getDataSlice action');
+  // we dont really use the key here
+  if (key === 'events') {
+    // uid == the event's ID
+    return eventsData[uid];
+  } else {
+    throw new Error('invalid key ' + key);
+  }
+};
+
+exports.createIndex = async (mainIndex = {}) => {
+  // do expensive initial fetches and cache them in .ssg/data.json
+  // console.log({ mainIndex: Object.keys(mainIndex.data) })
+  eventsData = mainIndex.yamlFiles['data-events-yml'].data.EventsList;
+  mainIndex.events = eventsData;
+  mainIndex.resources = mainIndex.yamlFiles['data-resources-yml'].data;
+  mainIndex.code = mainIndex.yamlFiles['data-code-yml'].data;
+  mainIndex.courses = mainIndex.yamlFiles['data-courses-yml'].data;
+  return mainIndex;
+};
+
+// optional lifecycle hook
+exports.postExport = async mainIndex => {
+  // eg for RSS
+  console.log('postExport', mainIndex)
+};
